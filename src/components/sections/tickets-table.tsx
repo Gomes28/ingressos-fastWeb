63473a297f150e2a1d2f40003ab4f3634e8e50b2
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-07 14:49:10 -0300

    Commit

--- src/components/sections/tickets-table.tsx
+++ src/components/sections/tickets-table.tsx
@@ -23,15 +23,15 @@ export interface SelectedTickets {
     quantity: number;
 }
 
-export function TicketsTable({tickets, event}: {tickets: Ticket[], event}) {
+export function TicketsTable({ tickets, event }: { tickets: Ticket[], event }) {
     const [selectedTickets, setSelectedTickets] = useState<SelectedTickets[]>([]);
     const [total, setTotal] = useState(0);
 
     const selectQuantity = (ticket, quantity) => {
         const exist = selectedTickets.find(item => item.ticket.id == ticket.id);
-        if(exist) {
+        if (exist) {
             const values = selectedTickets.map<SelectedTickets>(item => {
-                if(item.ticket.id == ticket.id) {
+                if (item.ticket.id == ticket.id) {
                     return {
                         quantity: +quantity,
                         ticket: ticket
@@ -42,7 +42,7 @@ export function TicketsTable({tickets, event}: {tickets: Ticket[], event}) {
             });
             setSelectedTickets(values);
         } else {
-            setSelectedTickets(old => [...old, {ticket, quantity}]);
+            setSelectedTickets(old => [...old, { ticket, quantity }]);
         }
     }
 
@@ -55,9 +55,9 @@ export function TicketsTable({tickets, event}: {tickets: Ticket[], event}) {
     }, [selectedTickets]);
 
     return (
-        <div id="tickets" className="hidden w-full max-w-8xl mx-auto lg:flex flex-col py-12">
+        <div id="tickets" className="w-full max-w-8xl mx-auto flex flex-col py-12 max-md:px-3">
             <h2 className="text-2xl font-bold text-gray-3 mb-6">Ingressos</h2>
-            <div className="border rounded-md">
+            <div className="border rounded-md max-md:hidden">
                 <div className="grid grid-cols-12 px-3 py-3 gap-4 border-b text-sm">
                     <div className="col-span-7">
                         <span>Ingressos</span>
@@ -111,7 +111,49 @@ export function TicketsTable({tickets, event}: {tickets: Ticket[], event}) {
                 }
                 )}
             </div>
-            <div className="border rounded-md mt-6">
+            <div className="border rounded-md">
+                <div className="grid grid-cols-12 px-3 py-3 gap-4 border-b text-sm">
+                    <div className="col-span-9">
+                        <span>Ingressos</span>
+                    </div>
+                    <div className="col-span-3">
+                        <span>Quant.</span>
+                    </div>
+                </div>
+                {tickets.map((ticket, index) => {
+                    const date = new Date(ticket.available_until);
+                    const [day, month, year] = date.toLocaleDateString().split('/');
+                    const values = [];
+                    const selected = selectedTickets.find(item => item.ticket.id == ticket.id);
+                    const total = selected ? (selected.quantity * (ticket.price + ticket.service_charge)).toString() : '000';
+                    for (let i = 0; i <= ticket.max_per_purchase; i++) {
+                        values.push(i);
+                    }
+                    return (
+                        <div className={`${index % 2 == 0 && 'bg-gray-100'} grid grid-cols-12 px-3 py-3 gap-4 ${index < tickets.length - 1 && 'border-b'} text-sm text-gray-5`}>
+                            <div className="col-span-9 flex flex-col">
+                                <div className="flex gap-4 items-center flex-wrap">
+                                    <h4 className="text-base font-medium text-gray-3">{ticket.name}</h4>
+                                    <div className="w-[6px] h-[6px] rounded-lg bg-black/30" />
+                                    <span className="text-black/60 text-sm">Taxa de serviço R$ {maskPrice(ticket.service_charge.toString())}</span>
+                                    <div className="w-[6px] h-[6px] rounded-lg bg-black/30" />
+                                    <span className="text-black/60 text-sm">Disponível até {day}/{month} às {date.getHours()}:{date.getMinutes()}</span>
+                                </div>
+                                <p className="text-xs">{ticket.description}</p>
+                            </div>
+                            <div className="col-span-3 flex items-center">
+                                <div className="h-12 w-20 px-3 rounded-md bg-white flex items-center border">
+                                    <select name="select" className="w-full outline-none h-full" onChange={e => selectQuantity(ticket, e.target.value)}>
+                                        {values.map(item => <option value={item}>{item}</option>)}
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+                )}
+            </div>
+            <div className="border rounded-md mt-6 max-md:hidden">
                 <div className="grid grid-cols-12 px-3 py-3 gap-4 border-b text-sm">
                     <div className="col-span-9 flex items-center">
                         <span className="text-gray-5">Subtotal R$ {maskPrice(total.toString())}</span>
@@ -134,7 +176,36 @@ export function TicketsTable({tickets, event}: {tickets: Ticket[], event}) {
                         <span className="text-lg font-semibold"></span>
                     </div>
                     <div className="col-span-2 flex items-center">
-                        <Link href={`/evento/checkout/${slugify(event.name, {lower: true})}/${event.id}`} className="h-12 px-8 bg-primary text-white rounded-md text-base font-medium flex gap-4 items-center"><FiLock />Comprar</Link>
+                        <Link href={`/evento/checkout/${slugify(event.name, { lower: true })}/${event.id}`} className="h-12 px-8 bg-primary text-white rounded-md text-base font-medium flex gap-4 items-center"><FiLock />Comprar</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="border rounded-md mt-6">
+                <div className="flex flex-col px-3 py-3 gap-4 border-b text-sm">
+                    <div className="col-span-9 flex items-center">
+                        <span className="text-gray-5">Subtotal R$ {maskPrice(total.toString())}</span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <div className="col-span-1">
+                            <span className="text-lg font-semibold">Total</span>
+                        </div>
+                        <div className="col-span-2">
+                            <span className="text-lg font-semibold">R$ {maskPrice(total.toString())}</span>
+                        </div>
+                    </div>
+                    <div className="col-span-2 flex items-center">
+                        <Link href={`/evento/checkout/${slugify(event.name, { lower: true })}/${event.id}`} className="h-12 w-full px-8 bg-primary text-white rounded-md text-base font-medium flex gap-4 items-center"><FiLock />Comprar</Link>
+                    </div>
+                </div>
+                <div className="grid grid-cols-12 px-3 py-3 gap-4 text-sm bg-gray-100">
+                    <div className="col-span-12 flex flex-col">
+                        <span className="text-gray-5">Formas de pagamento</span>
+                        <div className="flex flex-wrap gap-2 mt-2">
+                            {payments.map(item => <img src={item.image} alt="" key={item.id} />)}
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <span className="text-lg font-semibold"></span>
                     </div>
                 </div>
             </div>
