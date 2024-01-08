3868b8cec3824a92961f71928a2b522609606e2e
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-08 09:38:17 -0300

    Commit

--- src/components/user-wrapper.tsx
+++ src/components/user-wrapper.tsx
@@ -36,19 +36,19 @@ export function UserWrapper({ user }: { user: IUser }) {
                 <div className="absolute right-0 w-full min-h-28 border rounded-md bg-white top-[110%]">
                     <ul className="p-3 flex flex-col gap-2">
                         <li>
-                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md"><IoTicketOutline />Ingressos</Link>
+                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md text-sm"><IoTicketOutline size={20} />Ingressos</Link>
                         </li>
                         <li>
-                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md"><FiHeart />Favoritos</Link>
+                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md text-sm"><FiHeart size={20}/>Favoritos</Link>
                         </li>
                         <li>
-                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md"><FiUser />Minha conta</Link>
+                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md text-sm"><FiUser size={20}/>Minha conta</Link>
                         </li>
                         <li>
-                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md"><FiInfo />Central de ajuda</Link>
+                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md text-sm"><FiInfo size={20}/>Central de ajuda</Link>
                         </li>
                         <li>
-                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md"><FiLogOut />Sair</Link>
+                            <Link href={''} className="h-10 flex items-center gap-4 hover:bg-slate-100 transition-all bg-white px-3 rounded-md text-sm"><FiLogOut size={20}/>Sair</Link>
                         </li>
                     </ul>
                 </div>
