71bea398281dede4e8444788c99225351149cb97
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-09 17:46:23 -0300

    Commit

--- src/components/cards/card-ticket-purchase.tsx
+++ src/components/cards/card-ticket-purchase.tsx
@@ -20,7 +20,7 @@ export function CardTicketPurchase({ event }: { event: IEvent }) {
 
     return (
         <div className="flex flex-col shadow-lg rounded-md">
-            <div className="w-full h-[148px] overflow-hidden relative rounded-t-md">
+            <div className="w-full h-[148px] max-lg:h-[200px] overflow-hidden relative rounded-t-md">
                 <Image src={event.image} alt={event.name} fill />
             </div>
             <div className="flex flex-col p-3 border-b h-[81px]">
@@ -87,27 +87,27 @@ function Modal({ close }) {
 
     return (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black/50 z-[9999]">
-            <div className="w-full max-w-screen-lg h-[80vh] bg-white rounded-lg p-4 relative overflow-y-auto" ref={ref}>
+            <div className="w-full max-w-screen-lg max-lg:h-screen h-[80vh] bg-white lg:rounded-lg p-4 relative overflow-y-auto flex flex-col max-lg:items-center" ref={ref}>
                 <button onClick={close} className="h-10 w-10 flex items-center justify-center border border-gray-300 rounded-md absolute top-2 right-2"><FiX size={24} /></button>
                 <h2 className="text-xl font-semibold mt-8">Pedido nº 1G8VD4BQ9VQ</h2>
-                <div className="flex gap-4 items-center justify-between w-full mt-8">
-                    <div className="flex flex-col">
+                <div className="flex gap-4 max-lg:flex-col items-center justify-between w-full mt-8">
+                    <div className="flex flex-col max-lg:items-center">
                         <span className="text-xs">Enviado às 09h54 de 26/05/2022 a:</span>
                         <h4 className="mt-4 text-lg font-medium">Ezequiel Pires</h4>
                         <span className="text-sm font-light">ezequiel.pires08000@gmail.com</span>
                     </div>
-                    <div className="flex flex-col gap-2 items-center w-64 border-2 py-4 rounded-md bg-primary-light border-primary">
+                    <div className="flex flex-col gap-2 items-center w-64 border-2 py-4 rounded-md bg-primary-light border-primary max-lg:w-full">
                         <span className="text-sm">Nº DO PEDIDO:</span>
                         <span className="text-xl font-medium">1G8VD4BQ9VQ</span>
                         <strong className="text-primary-hover">Confirmado</strong>
                         <span className="text-lg">R$ {maskPrice(total.toString())}</span>
                     </div>
                 </div>
-                <h2 className="text-xl font-semibold mt-8">Ingressos comprados neste pedido</h2>
-                <div className="border border-black/20 rounded-md mt-4 p-6">
+                <h2 className="text-xl font-semibold mt-8 max-lg:text-center">Ingressos comprados neste pedido</h2>
+                <div className="border border-black/20 rounded-md mt-4 p-6 w-full">
                     {tickets.map((item, index) => (
-                        <div key={index} className={`flex justify-between items-center ${index < tickets.length - 1 && 'border-b'} gap-4 p-4`}>
-                            <div className="flex flex-col gap-1">
+                        <div key={index} className={`flex max-lg:flex-col justify-between items-center ${index < tickets.length - 1 && 'border-b'} gap-4 p-4`}>
+                            <div className="flex flex-col gap-1 max-lg:items-center">
                                 <span className="text-sm font-semibold text-gray-3">Ezequiel Pires</span>
                                 <span className="text-sm font-medium text-gray-500">ezequiel.pires@gmail.com</span>
                                 <span className="text-sm font-semibold text-gray-3">{item.name}</span>
